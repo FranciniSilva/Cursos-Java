a7587f666d2f8e10b7627b195394667e74baddab
FranciniSilva <franciniassis89@hotmail.com>
2016-09-07 16:58:49 -0300

    Operadores Matemáticos

--- Operador.java
+++ Operador.java
@@ -2,16 +2,8 @@ public class Operador {
 
 	public static void main(String[] args) {
 		
-		//2 + 3 operador binário
-		//-2 operador unário
-		//true ? "sim" : "não" operador ternário
-
-		//int x = 9 + 4;
-		String x = "9" + "4";
+		double x = 	7 + 3;
 		System.out.println(x);
-
-		double y = (7 - 4 + 3) * 2;
-		System.out.println(y);
 
 	}
 	
