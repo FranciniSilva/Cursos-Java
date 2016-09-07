1511727901e35cc90c1ca00fab1adc99f0072218
FranciniSilva <franciniassis89@hotmail.com>
2016-09-07 14:25:42 -0300

    Operadores

---
+++ Operador.java
@@ -0,0 +1,43 @@
+public class Operador {
+
+	public static void main(String[] args) {
+		
+		//2 + 3 operador binário
+		//-2 operador unário
+		//true ? "sim" : "não" operador ternário
+
+		//int x = 9 + 4;
+		String x = "9" + "4";
+		System.out.println(x);
+
+		double y = (7 - 4 + 3) * 2;
+		System.out.println(y);
+
+
+		//Operador         FINALIDADE
+		//+,-,*,?,%        Soma, subtrai, multiplica, divide, módulo - resto da divisão
+		//+                Concatena string
+		//+,-              Operador unário mais e menos
+		//<,>,<=,>=        Comparar, menor que, maior que,menor igual que, maior igual que
+		//++,--            Pré ou pós incremento, pré ou pós decremento
+		//==,!=            Comparar se é igual ou diferente
+		//&&,||            Concatenção lógica e, concatenação lógica ou
+		//.                Acessar propriedades de um objeto
+		//[]               Indexar Arrays
+		//()               Chamar método
+		//?:               Ternário, retorna booleano
+		//<<,>>,>>>        Operar bit numérico
+		//~                Complemento unário sobre bits
+		//!                Complemento lógico negação
+		//&,|,^            OPerador bit AND, OR XOR
+		//,                Separar valores
+		//+=,-=,*=,/=      Atribuir valores somando, atribuir valores subtraindo, atribuir valores multiplicando, atribuir valores dividindo
+		//<<=,>>=,>>>=     Operador de bytes (unário)
+		//&=,|=,^=         Operador de bytes AND, OR XOR, (unário)
+		//instanceof       Verificar o tipo do objeto
+		//new              Criar objeto
+		//(tipo)           Coerção unária
+
+	}
+	
+}
