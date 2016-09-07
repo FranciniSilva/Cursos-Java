4a9e6ca2e6649898282520c7ba5fda7b2b147c63
FranciniSilva <franciniassis89@hotmail.com>
2016-09-07 13:32:42 -0300

    Entrada gráfica de dados com JOptionPane

--- Entrada.java
+++ Entrada.java
@@ -1,17 +1,12 @@
 import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 public class Entrada {
 	
 	public static void main(String[] args) {
 		
-		//Chamada do programa
-		//System.out.println(args[0]);
-
-		//Interagindo com o usuário
-		Scanner s = new Scanner(System.in);
-		System.out.println("Qual o seu nome?");
-		String nome = s.nextLine();
-		System.out.println("Bem-vindo " + nome);
-		//System.out.println(s.nextLine()); 
+		String nome = JOptionPane.showInputDialog("Qual é o seu nome?");
+		//System.out.println(nome);
+		JOptionPane.showMessageDialog(null, nome);
 	}
 }
