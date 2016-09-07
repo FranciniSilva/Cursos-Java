113a9d036996b89c69d42788087758321fe19e16
FranciniSilva <franciniassis89@hotmail.com>
2016-09-07 17:08:58 -0300

    Módulo - Resto divisão

--- Operador.java
+++ Operador.java
@@ -2,9 +2,9 @@ public class Operador {
 
 	public static void main(String[] args) {
 		
-		double x = 	7 + 3;
-		String y = "OI " + " PROGRAMADOR JAVA";
-		System.out.println(y);
+		double x = 	7 % 3;
+
+		System.out.println(x);
 
 	}
 	
