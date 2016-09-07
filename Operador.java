afcb25b9c913af107c769119109c4f3fcb560f67
FranciniSilva <franciniassis89@hotmail.com>
2016-09-07 17:22:34 -0300

    Operador de pós incremento

--- Operador.java
+++ Operador.java
@@ -3,7 +3,7 @@ public class Operador {
 	public static void main(String[] args) {
 		
 		double x = 	6;
-		double y = ++x;
+		double y = x++;//pós incremento
 
 		System.out.println("x=" +x);
 		System.out.println("y=" +y);
