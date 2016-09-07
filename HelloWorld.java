0a1a24082efbe19747428b1648bbe1fec9cb5939
FranciniSilva <franciniassis89@hotmail.com>
2016-09-06 23:38:47 -0300

    Variaveis primitivas

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("\tOi Mundo!");
+		System.out.println("Java \nSE");
+	}
+}
---
+++ Variavel.java
@@ -0,0 +1,22 @@
+/**
+ * Apresentar os conceitos de Variável
+ *@author Francini de Assis Silva
+*/
+public class Variavel {
+	
+	public static void main(String[] args) {
+		
+		String nome = "Francini"; 
+		int idade = 26;
+		boolean casado = false;
+		double preco = 12.45;
+		char sexo = 'F';
+
+
+		System.out.println(nome);
+		System.out.println(idade);
+		System.out.println(casado);
+		System.out.println(preco);
+		System.out.println(sexo);
+	}
+}
