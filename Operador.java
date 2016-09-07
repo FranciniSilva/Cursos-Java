7c4e8ae82931a31e0ca161dcd6e05ac290b3a680
FranciniSilva <franciniassis89@hotmail.com>
2016-09-07 20:30:44 -0300

    Operadores de Atribuição

--- Operador.java
+++ Operador.java
@@ -2,11 +2,15 @@ public class Operador {
 
 	public static void main(String[] args) {
 		
-		Integer x = 6;
+		int x = 6;
 
-		System.out.println((x >= 1) && (x <= 10));
-		System.out.println((x >= 1) || (x <= 5));
-		System.out.println(!(x >= 1)); //operador de negação
+		x %= 3;
+		x += 3;
+		x -= 3;
+		x /= 3;
+		x *= 3;
+
+		System.out.println(x);
 	}
 	
 }
