74a958fb79e1929ad5d8b159b8404d63af6e0b60
FranciniSilva <franciniassis89@hotmail.com>
2016-09-07 21:07:41 -0300

    Operadores Especiais 2

--- Operador.java
+++ Operador.java
@@ -2,12 +2,24 @@ public class Operador {
 
 	public static void main(String[] args) {
 		
-		int idade = 20;
+		Scanner s = new Scanner(System.in);
+		System.out.println("Informe o raio");
+		double raio = s.nextDouble();
 
-		String x = (idade >= 18) ? "Maior de idade" : "Menor de idade";
+		//Diametro : 2r
+		//double raio = 10;
+		double diametro = 2 * raio;
 
-		String sexo = "M", pais = "Brasil";
-		System.out.println(x);
+		System.out.println("Diametro = " + diametro);
+
+		//Circunferencia : 2 PI r
+		double pi = Math.PI;
+		double circunferencia = 2 * pi * raio;
+		System.out.println("Circunferencia = " + circunferencia);
+
+		//Area PI r2
+		double area = pi * (raio * raio);
+		System.out.println("Area = " + area);
 	}
 	
 }
