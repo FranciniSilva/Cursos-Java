c71fecc865242d2e845633c2a5167e630bc7f668
FranciniSilva <franciniassis89@hotmail.com>
2016-09-07 21:06:40 -0300

    Operadores especiais 01

--- Operador.java
+++ Operador.java
@@ -2,14 +2,11 @@ public class Operador {
 
 	public static void main(String[] args) {
 		
-		int x = 6;
+		int idade = 20;
 
-		x %= 3;
-		x += 3;
-		x -= 3;
-		x /= 3;
-		x *= 3;
+		String x = (idade >= 18) ? "Maior de idade" : "Menor de idade";
 
+		String sexo = "M", pais = "Brasil";
 		System.out.println(x);
 	}
 	
