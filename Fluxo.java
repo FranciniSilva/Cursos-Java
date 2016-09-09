95ea1ca9c7eeda2e0fd1573ffc57e603d764d015
FranciniSilva <franciniassis89@hotmail.com>
2016-09-09 20:37:49 -0300

    Fluxo aninhado

--- Fluxo.java
+++ Fluxo.java
@@ -1,15 +1,14 @@
 public class Fluxo{
 	public static void main(String[] args) {
 		
-		int idade = 63;
-		if (idade <= 11) {
-			System.out.println("Crianca");
-		}else if (idade > 11 && idade <= 18){
-			System.out.println("Adolescente");
-		} else if (idade > 18 && idade <= 60) {
-			System.out.println("Adulto");
+		int nota = 6;
+		if (nota >= 7) {
+			System.out.println("Passou");
 		} else {
-			System.out.println("Melhor idade");
+			System.out.println("Reprovou");
+			if (nota >= 6) {
+				System.out.println("mas pode recuperar a nota");
+			}
 		}
 	}
 }
