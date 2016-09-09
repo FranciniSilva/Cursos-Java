12c6864d1c6b1545843ec5bb7315dd1e3ddb662d
FranciniSilva <franciniassis89@hotmail.com>
2016-09-09 20:11:29 -0300

    Fluxo

---
+++ Fluxo.java
@@ -0,0 +1,11 @@
+public class Fluxo{
+	public static void main(String[] args) {
+		
+		int idade = 5;
+		if ((numero % 2) == 0) {
+			System.out.println("Par");
+		}else {
+			System.out.println("Impar");
+		}
+	}
+}
