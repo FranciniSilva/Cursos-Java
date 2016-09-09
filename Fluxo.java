95a1c45f32ced07be63d3d5a6e4ea704c1466d9e
FranciniSilva <franciniassis89@hotmail.com>
2016-09-09 20:32:19 -0300

    Fluxo 02

--- Fluxo.java
+++ Fluxo.java
@@ -1,11 +1,15 @@
 public class Fluxo{
 	public static void main(String[] args) {
 		
-		int idade = 5;
-		if ((numero % 2) == 0) {
-			System.out.println("Par");
-		}else {
-			System.out.println("Impar");
+		int idade = 63;
+		if (idade <= 11) {
+			System.out.println("Crianca");
+		}else if (idade > 11 && idade <= 18){
+			System.out.println("Adolescente");
+		} else if (idade > 18 && idade <= 60) {
+			System.out.println("Adulto");
+		} else {
+			System.out.println("Melhor idade");
 		}
 	}
 }
