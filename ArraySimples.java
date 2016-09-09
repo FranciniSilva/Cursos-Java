c4faad4014d3442201b2c956f10e74857594a9ce
FranciniSilva <franciniassis89@hotmail.com>
2016-09-09 18:33:51 -0300

    Array Multidimensional

--- ArraySimples.java
+++ ArraySimples.java
@@ -4,23 +4,16 @@ public class ArraySimples {
 
 	public static void main(String[] args) {
 
-		
-		String[] paises = {"Brasil", "Russia", "India", "China"};
-		System.out.println(paises[0]);
-		paises[0] = "BRAZIL";
-		System.out.println(paises[0]);
-		System.out.println(paises.length);
-		System.out.println(Arrays.toString(paises));
+		String[] uma = {"Francini", "Ricardo"};
+		System.out.println(uma[0]);
 
-		//pesquisa
+		String[][] duas = {{"Francini", "F", "SP"},{"Ricardo", "M", "SP"}};
 
-		int posicao = Arrays.binarySearch(paises,"Russia");
-		System.out.println(posicao);
+		duas[1][0] = "SANDRA";
 
-		Arrays.sort(paises, 0, paises.length);
-		System.out.println(Arrays.toString(paises));	
 
-		Double[] valores = {12.35, 3456.3456};
-		System.out.println(valores[0].doubleValue());
+		System.out.println(duas[1][0]);
+
+
 	}
 }
