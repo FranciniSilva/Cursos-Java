8ede517c82c2ae65b1df38f190ad5813074a3d5a
FranciniSilva <franciniassis89@hotmail.com>
2016-09-09 18:17:19 -0300

    Inicializando Array e a classe Arrays do java

--- ArraySimples.java
+++ ArraySimples.java
@@ -1,7 +1,26 @@
+import java.util.Arrays;
+
 public class ArraySimples {
 
 	public static void main(String[] args) {
+
 		
-		
+		String[] paises = {"Brasil", "Russia", "India", "China"};
+		System.out.println(paises[0]);
+		paises[0] = "BRAZIL";
+		System.out.println(paises[0]);
+		System.out.println(paises.length);
+		System.out.println(Arrays.toString(paises));
+
+		//pesquisa
+
+		int posicao = Arrays.binarySearch(paises,"Russia");
+		System.out.println(posicao);
+
+		Arrays.sort(paises, 0, paises.length);
+		System.out.println(Arrays.toString(paises));	
+
+		Double[] valores = {12.35, 3456.3456};
+		System.out.println(valores[0].doubleValue());
 	}
 }
